e86659d8df07a7defbfe24da57b6c5683dd77aaf
dimitrod <dimtrod@googlemail.com>
2024-12-08 19:52:03 +0100

    Modifying model class

---
+++ MistralInstruct/mistral_instruct.py
@@ -0,0 +1,29 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+class tinyllama:
+  def __init__(self):
+    quantization_config = BitsAndBytesConfig(load_in_4bit=True)
+    model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+
+    model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config, device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+  def invoke(self, question):
+    device = "cuda:0"
+    question_add = " Answer as shortly as possible, no additional information, no punctiation."
+    instruction = "You are a chatbot who always responds as shortly as possible."
+
+    messages = [
+        {
+            "role": "system",
+            "content": instruction,
+        },
+        {"role": "user", "content": question + question_add},
+    ]
+
+    inputs = self.tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
+    outputs = self.model.generate(inputs, max_new_tokens=50)
+    result = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+    result = result.replace(question + question_add + " ", "")
+    result = result.replace(instruction, "")
+    return result
