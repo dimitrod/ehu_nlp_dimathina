7ca64c9eaf8a670e3956bbdd5bc73fa4f3b0c788
Dimitar Dimitrov <91436271+dimitrod@users.noreply.github.com>
2024-12-09 22:22:16 +0100

    Update mistral_instruct.py
    Fixing errors

--- MistralInstruct/mistral_instruct.py
+++ MistralInstruct/mistral_instruct.py
@@ -1,10 +1,16 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from huggingface_hub import login
+import torch
 
 class mistral_instruct:
   def __init__(self, params):
     login()
-    quantization_config = BitsAndBytesConfig(load_in_4bit=True)
+    quantization_config = BitsAndBytesConfig(
+      load_in_4bit=True,
+      bnb_4bit_use_double_quant=True,
+      bnb_4bit_quant_type="nf4",
+      bnb_4bit_compute_dtype=torch.bfloat16
+    )
     model_id = "mistralai/Mistral-7B-Instruct-v0.3"
 
     self.model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config, device_map="auto")
