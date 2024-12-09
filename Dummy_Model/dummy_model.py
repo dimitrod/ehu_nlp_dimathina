2096fd7b7728e8ae7e43e1d4aa13b4c26ae856d1
matthiasw96 <148684037+matthiasw96@users.noreply.github.com>
2024-12-09 08:42:14 +0100

    Update dummy_model.py
    setting multiple parameters

--- Dummy_Model/dummy_model.py
+++ Dummy_Model/dummy_model.py
@@ -2,7 +2,9 @@ import json
 
 class dummy_model:
     def __init__(self, params):
-        print(params[0])
+        print("This is param 1: " + params[0])
+        print("This is param 2: " + params[1])
+        
         with open("Dummy_Model/dummy_data.json", "r", encoding="utf-8") as f:
             self.data = json.loads(f.read())
 
