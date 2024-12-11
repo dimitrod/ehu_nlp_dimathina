4803a9497202df812a71a0a992ec16444339b810
KatharinaRausch <74824741+KatharinaRausch@users.noreply.github.com>
2024-12-11 19:44:47 +0100

    Update mistral_instruct.py

--- MistralInstruct/mistral_instruct.py
+++ MistralInstruct/mistral_instruct.py
@@ -1,29 +1,23 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from huggingface_hub import login
 from sentence_transformers import SentenceTransformer
 from pinecone import Pinecone, ServerlessSpec
 import torch
+from transformers import pipeline
 
 class mistral_instruct:
   def __init__(self, params):
     API_KEY = input('Enter Pinecone.io token: ')
     login()
 
-    quantization_config = BitsAndBytesConfig(
-      load_in_4bit=True,
-      bnb_4bit_use_double_quant=True,
-      bnb_4bit_quant_type="nf4",
-      bnb_4bit_compute_dtype=torch.bfloat16
-    )
-    model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+    model_id = 'distilbert/distilbert-base-cased-distilled-squad'
 
     self.retriever = SentenceTransformer('sentence-transformers/multi-qa-MiniLM-L6-cos-v1')
-    self.model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config, device_map="auto")
-    self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+    self.model = pipeline("question-answering", model = model_id)
     self.pc = Pinecone(api_key=API_KEY)
+    
 
   def invoke(self, question):
-    device = "cuda:0"
     question_add = " Answer in one or two words, no additional information, no punctiation. Use the following text to find the answer:"
     instruction = "You are a chatbot who always responds as shortly as possible."
     question_context = ""
@@ -35,17 +29,7 @@ class mistral_instruct:
     for x in query_return['matches']:
       question_context = question_context + str(x['metadata']['bytes'])
 
-    messages = [
-        {
-            "role": "system",
-            "content": instruction,
-        },
-        {"role": "user", "content": question + question_add + question_context},
-    ]
+    answer = self.model(question = question, context = question_context)
 
-    inputs = self.tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
-    outputs = self.model.generate(inputs, max_new_tokens=20)
-    result = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-    result = result.replace(question + question_add + question_context + " ", "")
-    result = result.replace(instruction, "")
     return result
+
