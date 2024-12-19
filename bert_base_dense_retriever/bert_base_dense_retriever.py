120fbd9883437a44e43b51485f4f21b529913e23
dimitrod <dimtrod@googlemail.com>
2024-12-19 13:14:57 +0100

    Bert base dense retrieval cleanup

---
+++ bert_base_dense_retriever/bert_base_dense_retriever.py
@@ -0,0 +1,32 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from huggingface_hub import login
+from sentence_transformers import SentenceTransformer
+from pinecone import Pinecone, ServerlessSpec
+import torch
+from transformers import pipeline
+
+class bert_base_dense_retriever:
+  def __init__(self, params):
+    API_KEY = input('Enter Pinecone.io token: ')
+    login()
+
+    model_id = 'distilbert/distilbert-base-cased-distilled-squad'
+
+    self.retriever = SentenceTransformer('sentence-transformers/multi-qa-MiniLM-L6-cos-v1')
+    self.model = pipeline("question-answering", model = model_id)
+    self.pc = Pinecone(api_key=API_KEY)
+    
+
+  def invoke(self, question):
+    question_context = ""
+    index = self.pc.Index('wiki-train-minilm')
+    query = question
+    query_encoded = self.retriever.encode([query]).tolist()
+    query_return = index.query(vector=query_encoded, top_k=2, include_metadata=True)
+    for x in query_return['matches']:
+      question_context = question_context + str(x['metadata']['bytes'])
+
+    answer = self.model(question = question, context = question_context)
+
+    return answer["answer"]
+
