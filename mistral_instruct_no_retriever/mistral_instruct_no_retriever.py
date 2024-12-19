5ee0254f7b9d82e4199e7c1be2ad8281e46da6ff
dimitrod <dimtrod@googlemail.com>
2024-12-19 11:18:59 +0100

    Cleanup mistral no retriever

---
+++ mistral_instruct_no_retriever/mistral_instruct_no_retriever.py
@@ -0,0 +1,37 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from huggingface_hub import login
+import torch
+
+class mistral_instruct_no_retriever:
+  def __init__(self, params):
+    login()
+    quantization_config = BitsAndBytesConfig(
+      load_in_4bit=True,
+      bnb_4bit_use_double_quant=True,
+      bnb_4bit_quant_type="nf4",
+      bnb_4bit_compute_dtype=torch.bfloat16
+    )
+    model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+
+    self.model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config, device_map="auto")
+    self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+  def invoke(self, question):
+    device = "cuda:0"
+    question_add = " Answer as shortly as possible, no additional information, no punctiation."
+    instruction = "You are a chatbot who always responds as shortly as possible."
+
+    messages = [
+        {
+            "role": "system",
+            "content": instruction,
+        },
+        {"role": "user", "content": question + question_add},
+    ]
+
+    inputs = self.tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
+    outputs = self.model.generate(inputs, max_new_tokens=50)
+    result = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+    result = result.replace(question + question_add + " ", "")
+    result = result.replace(instruction, "")
+    return result
